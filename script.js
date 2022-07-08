d1a2d979706fa0d07e710a55b9248a7ff4a2842f
HankFontaine <henrymcgill@gmail.com>
2022-07-07 20:39:23 -0400

    started fixing bugs

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let myLibrary = [];
 
 const shining = new Book("Shining", "Stephen King", 596, "have read");
 const notebook = new Book("The notebook", "mike", 56, "have not read");
+const bible = new Book("bible", "God", 1000, "have read");
 // const userBook = new Book(
 //   prompt("title?"),
 //   prompt("author?"),
@@ -26,17 +27,18 @@ function addBookToLibrary(newBook) {
 
 addBookToLibrary(shining);
 addBookToLibrary(notebook);
+addBookToLibrary(bible);
 // addBookToLibrary(userBook);
 
 //should be a nested function to create new divs within card div every time
-document.querySelector(".book-title").innerHTML =
-  "TITLE: " + myLibrary[1].title;
-document.querySelector(".book-author").innerHTML =
-  "AUTHOR: " + myLibrary[1].author;
-document.querySelector(".book-pages").innerHTML =
-  "PAGES: " + myLibrary[1].pageNumber;
-document.querySelector(".book-have-read").innerHTML =
-  "HAVE I READ IT?: " + myLibrary[1].haveRead;
+// document.querySelector(".book-title").innerHTML =
+//   "TITLE: " + myLibrary[1].title;
+// document.querySelector(".book-author").innerHTML =
+//   "AUTHOR: " + myLibrary[1].author;
+// document.querySelector(".book-pages").innerHTML =
+//   "PAGES: " + myLibrary[1].pageNumber;
+// document.querySelector(".book-have-read").innerHTML =
+//   "HAVE I READ IT?: " + myLibrary[1].haveRead;
 
 for (i = 0; i < totalBooks; i++) {
   let newDiv = document.createElement("div");
