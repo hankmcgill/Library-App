23f10dbb8a44b32fc2d5cceea99b13c05a272369
HankFontaine <henrymcgill@gmail.com>
2022-07-10 13:13:48 -0400

    started work on layout and functionality

--- script.js
+++ script.js
@@ -19,8 +19,7 @@ function newEntry() {
   const userBook = new Book(
     prompt("title?"),
     prompt("author?"),
-    prompt("page number?"),
-    prompt("have you read it?")
+    prompt("page number?")
   );
   addBookToLibrary(userBook);
   clearContainer();
@@ -31,33 +30,47 @@ function clearContainer() {
   container.innerHTML = "";
 }
 
-//should be a nested function to create new divs within card div every time
-// document.querySelector(".book-title").innerHTML =
-//   "TITLE: " + myLibrary[1].title;
-// document.querySelector(".book-author").innerHTML =
-//   "AUTHOR: " + myLibrary[1].author;
-// document.querySelector(".book-pages").innerHTML =
-//   "PAGES: " + myLibrary[1].pageNumber;
-// document.querySelector(".book-have-read").innerHTML =
-//   "HAVE I READ IT?: " + myLibrary[1].haveRead;
+function makeDivContents(title, author, pages) {
+  const newDiv = document.createElement("div");
+  newDiv.className = "book-card";
+  const deleteBook = document.createElement("button");
+  deleteBook.className = "delete-button";
+  deleteBook.append("X");
+  newDiv.appendChild(deleteBook);
+  const newBookTitle = document.createElement("h2");
+  newBookTitle.className = "book-text";
+  newBookTitle.append(title);
+  newDiv.appendChild(newBookTitle);
+  const newBookAuthor = document.createElement("div");
+  newBookAuthor.className = "book-text";
+  newBookAuthor.append("AUTHOR: " + author);
+  newDiv.appendChild(newBookAuthor);
+  const newBookPageNumber = document.createElement("div");
+  newBookPageNumber.className = "book-text";
+  newBookPageNumber.append("PAGE NUMBER: " + pages);
+  newDiv.appendChild(newBookPageNumber);
+  const newBookHaveRead = document.createElement("button");
+  newBookHaveRead.className = "unread-button";
+  newBookHaveRead.className = "book-text";
+  newBookHaveRead.append("I HAVEN'T READ THIS!");
+  newBookHaveRead.onclick = function () {
+    // if (newBookHaveRead.className === "unread-button") {
+    // newBookHaveRead.className === "read-button";
+    newBookHaveRead.innerHTML = "I READ IT ALREADY!";
+    console.log("test");
+    // }
+  };
+
+  newDiv.appendChild(newBookHaveRead);
+  container.prepend(newDiv);
+}
 
 function makeList() {
   for (i = 0; i < totalBooks; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.id = "div" + i;
-    newDiv.className = "book-card";
-    console.log(newDiv);
-    newDiv.innerHTML =
-      "TITLE: " +
-      myLibrary[i].title +
-      "AUTHOR: " +
-      myLibrary[i].author +
-      "PAGES: " +
-      myLibrary[i].pageNumber +
-      "HAVE I READ IT?: " +
-      myLibrary[i].haveRead;
-    container.prepend(newDiv);
+    makeDivContents(
+      myLibrary[i].title,
+      myLibrary[i].author,
+      myLibrary[i].pageNumber
+    );
   }
 }
-
-// makeList();
