b0196e82aedf8b40cb898fc413b3b029290dcd33
hankfontaine <hankfontaine@gmail.com>
2023-01-04 22:37:50 -0500

    added a README

---
+++ README.md
@@ -0,0 +1,15 @@
+# Library App
+
+### Live Preview:
+https://hankfontaine.github.io/Library-App/
+
+## Introduction
+I made this CRUD app as part of The Odin Project's JavaScript course.  I tried to design the app in a way that implements array methods and also shows my own unique sense of style.  If using a sleek comic-book inspired aesthetic is a crime - lock me up and throw away the key!
+
+## Features
+I wrote this app using vanilla JavaScript.
+
+#### Contact
+Got questions, feedback, or a request? Email me at henrymcgill@gmail.com, or feel free to reach out to me on Twitter: @hankfontaine.
+
+https://hankfontaine.com
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let container = document.querySelector(".container");
-let bookCardDiv = document.querySelector(".book-card");
+let container = document.querySelector('.container');
+let bookCardDiv = document.querySelector('.book-card');
 let currentBookTotal = 0;
 let myLibrary = [];
 let allTimeBookTotal = 0;
@@ -30,39 +30,39 @@ const subtractBookFromLibrary = (value) => {
 
 const newEntry = () => {
   const userBook = new Book(
-    prompt("title?"),
-    prompt("author?"),
-    prompt("page number?")
+    prompt('title?'),
+    prompt('author?'),
+    prompt('page number?'),
   );
   addBookToLibrary(userBook);
   makeList();
 };
 
 const makeDivContents = (book) => {
-  const newDiv = document.createElement("div");
-  newDiv.className = "book-card";
-  const deleteButton = document.createElement("button");
-  deleteButton.className = "delete-button";
+  const newDiv = document.createElement('div');
+  newDiv.className = 'book-card';
+  const deleteButton = document.createElement('button');
+  deleteButton.className = 'delete-button';
   deleteButton.onclick = function () {
     subtractBookFromLibrary(book.id);
   };
-  deleteButton.append("X");
+  deleteButton.append('X');
   newDiv.appendChild(deleteButton);
-  const newBookTitle = document.createElement("h2");
-  newBookTitle.className = "book-text";
+  const newBookTitle = document.createElement('h2');
+  newBookTitle.className = 'book-text';
   newBookTitle.append(book.title);
   newDiv.appendChild(newBookTitle);
-  const newBookAuthor = document.createElement("div");
-  newBookAuthor.className = "book-text";
-  newBookAuthor.append("AUTHOR: " + book.author);
+  const newBookAuthor = document.createElement('div');
+  newBookAuthor.className = 'book-text';
+  newBookAuthor.append('AUTHOR: ' + book.author);
   newDiv.appendChild(newBookAuthor);
-  const newBookPageNumber = document.createElement("div");
-  newBookPageNumber.className = "book-text";
-  newBookPageNumber.append("PAGE NUMBER: " + book.pageNumber);
+  const newBookPageNumber = document.createElement('div');
+  newBookPageNumber.className = 'book-text';
+  newBookPageNumber.append('PAGE NUMBER: ' + book.pageNumber);
   newDiv.appendChild(newBookPageNumber);
-  const newBookHaveRead = document.createElement("button");
-  newBookHaveRead.className = "unread-button";
-  newBookHaveRead.className = "book-text";
+  const newBookHaveRead = document.createElement('button');
+  newBookHaveRead.className = 'unread-button';
+  newBookHaveRead.className = 'book-text';
   newBookHaveRead.append("I HAVEN'T READ THIS!");
   newBookHaveRead.onclick = function () {
     book.haveRead = !book.haveRead;
@@ -75,7 +75,7 @@ const makeDivContents = (book) => {
 };
 
 const makeList = () => {
-  container.innerHTML = "";
+  container.innerHTML = '';
   for (i = 0; i < currentBookTotal; i++) {
     makeDivContents(myLibrary[i]);
   }
